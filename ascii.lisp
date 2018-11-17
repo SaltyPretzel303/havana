a2f0f2f8db3593d22df8223d4318bc434ef3b4ed
Nemanja <nemanja.m.303@gmail.com>
2018-11-17 21:10:46 +0100

    matrix created

---
+++ ascii.lisp
@@ -0,0 +1,7 @@
+(setq ascii '(A B C D E F G H I J K L M N O P Q R S #\T U V W X Y Z))
+
+(defun get_ascii(index)
+  (cond
+    ((> index 26) '())
+    ((< index 0) '())
+    (t (nth index ascii))))
--- readme.txt
+++
@@ -1,1 +0,0 @@
-pomozi boze
