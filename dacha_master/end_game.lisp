f4615861e0eaf41e807601c9cdc71ab80f1805f2
Nemanja <nemanja.m.303@gmail.com>
2018-11-28 15:24:38 +0100

    corners

--- dacha_master/end_game.lisp
+++ dacha_master/end_game.lisp
@@ -10,58 +10,30 @@
 (defun find_fork(symbol row column)
   ())
 
-; (setq coords '((0 0)                                               ;gore levo
-;               (0 (1- (get_mat_dim matrix)))                        ;gore desno
-;               ((1- (get_mat_dim matrix)) 0)                               ;levo
-;               ((1- (get_mat_dim matrix)) (* 2 (1- (get_mat_dim matrix))))        ;desno
-;               ((* 2 (1- (get_mat_dim matrix))) (1- (get_mat_dim matrix)))        ;dole levo
-;               ((* 2 (1- (get_mat_dim matrix))) (*2 (1- (get_mat_dim matrix))))   ;dole desno
-;               ))
+; in variable corners sets coordinats of all corners
+(setq corners (cons (list '0 '0) ; upper left
+                (cons (list '0 (1- (get_mat_dim matrix))) ; upper right
+                    (cons (list (1- (get_mat_dim matrix)) '0) ; middle left
+                          (cons (list (1- (get_mat_dim matrix)) (- (* 2 (get_mat_dim matrix)) 2)) ; middle right
+                                (cons (list (- (* 2 (get_mat_dim matrix)) 2) (1- (get_mat_dim matrix))) ; lower left
+                                      (cons (list (- (* 2 (get_mat_dim matrix)) 2) (- (* 2 (get_mat_dim matrix)) 2)) '())))))));lower right
 
-(defun check_corners(symbol)
-  (progn
-        (setq corners '(#\#))
-        (cond
-                   ((equalp (cadr (get_element 0 0)) symbol) (append corners #\#))
-                   (t '()))
-        (cond
-                   ((equalp (cadr (get_element 0 (1- (get_mat_dim matrix)))) symbol) (cons corners (list 0 (1- (get_mat_dim matrix)))))
-                   (t '()))
-        (cond
-                   ((equalp (cadr (get_element (1- (get_mat_dim matrix)) 0)) symbol) (cons corners (list (1- (get_mat_dim matrix)) 0)))
-                   (t '()))
-        (cond
-                   ((equalp (cadr (get_element (1- (get_mat_dim matrix)) (* 2 (1- (get_mat_dim matrix))))) symbol) (cons (list (1- (get_mat_dim matrix)) (* 2 (1- (get_mat_dim matrix))))))
-                   (t '()))
-        (cond
-                   ((equalp (cadr (get_element (* 2 (1- (get_mat_dim matrix))) (1- (get_mat_dim matrix)))) symbol) (cons (list (* 2 (1- (get_mat_dim))) (1- (get_mat_dim)))))
-                   (t '()))
-        (cond
-                   ((equalp (cadr (get_element (* 2 (1- (get_mat_dim matrix))) (* 2 (1- (get_mat_dim matrix))))) symbol) (cons (list (* 2 (1- (get_mat_dim matrix))) (* 2 (1- (get_mat_dim matrix))))))
-                   (t '()))
-        ))
+(defun remove_clear_corners (symbol ls)
+  (cond
+    ((null ls) '())
+    (t (if (equalp (cadr (get_element (car (car ls))(cadr (car ls)))) symbol)
+          (cons (car ls) (remove_clear_corners symbol (cdr ls)))
+         (remove_clear_corners symbol (cdr ls))))))
 
-(defun check_corners2(symbol)
-  (progn
-   (setq corners '())
-   (if (equalp (cadr (first (cadr (first matrix)))) symbol)
-     (cons (list '0 '0) corners)
-     )
-   (if (equalp (cadr (last (cadr (first matrix)))) symbol)
-     (append (list '0 (1- (get_mat_dim matrix))) corners))
-   (if (equalp (cadr (first (cadr (nth (1- (get_mat_dim matrix)) matrix)))) symbol)
-     (append (list (1- (get_mat_dim matrix)) '0) corners))
-   (if (equalp (cadr (last (cadr (nth (1- (get_mat_dim matrix)) matrix)))) symbol)
-     (cons (list (1- (get_mat_dim matrix)) (- (* 2 (get_mat_dim matrix)) 2)) corners))
-   (if (equalp (cadr (first (cadr (last matrix)))) symbol)
-     (cons (list (- (* 2 (get_mat_dim matrix)) 2) (1- (get_mat_dim matrix)) ) corners))
-   (if (equalp (cadar (last (cadr (nth 10 matrix)))) symbol)
-     (list (list (- (* 2 (get_mat_dim matrix)) 2) (- (* 2 (get_mat_dim matrix)) 2)) corners)
-     )
-   ))
-
-
-(make_move 'X '0 '0)
-(make_move 'X (- (* 2 (get_mat_dim matrix)) 2) (- (* 2 (get_mat_dim matrix)) 2))
+(make_move 'O '0 '0) ; 0 0
+(make_move 'X  '0 (1- (get_mat_dim matrix))) ; 0 5
+(make_move 'O (1- (get_mat_dim matrix)) '0) ; 5 0
+(make_move 'X (1- (get_mat_dim matrix)) (- (* 2 (get_mat_dim matrix)) 2)) ; 5 10
+(make_move 'X (- (* 2 (get_mat_dim matrix)) 2) (1- (get_mat_dim matrix))) ; 10 5
+(make_move 'O (- (* 2 (get_mat_dim matrix)) 2) (- (* 2 (get_mat_dim matrix)) 2)) ; 10 10
 (print_matrix)
-(princ (check_corners2 'X))
+
+(princ #\linefeed)
+(princ corners)
+(princ #\linefeed)
+(princ (remove_clear_corners 'X corners))
